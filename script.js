cef543658e8fff3f7d87f0e57d7c9dcf29d5a356
Ruslana Kolykhan <ruslana7070@gmail.com>
2025-07-28 23:10:04 +0300

    js done button animation

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function randomCatImages() {
+    const   button = document.querySelector(".btn");
+    const image = document.querySelector("img");
 
+    async function randomCatImages() {
+        try {
+            const response = await fetch("https://api.thecatapi.com/v1/images/search");
+            const data = await response.json();
+            image.src = data[0].url;
+
+        }
+        catch (error) {
+            console.log(error);
+        }
 }
+ button.addEventListener("click", () => {
+     let isLoaded = image.complete;
+     if (isLoaded) {
+         randomCatImages();
+     }
+ });
